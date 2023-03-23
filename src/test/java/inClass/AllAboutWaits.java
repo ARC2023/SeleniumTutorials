68a5f9911ea79590f22fff0a1432706fa26a0dc8
ARC2023 <arcautomation2023@gmail.com>
2023-03-23 10:00:56 +0530

    Waits In progress 23 March 2023

--- src/test/java/inClass/AllAboutWaits.java
+++ src/test/java/inClass/AllAboutWaits.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,19 +19,21 @@ import io.netty.util.internal.StringUtil;
 
 public class AllAboutWaits {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ChromeOptions option = new ChromeOptions();
 		option.addArguments("--start-maximized");
 		option.setExperimentalOption("excludeSwitches", Arrays.asList("disable-popup-blocking", "enable-automation"));
 		option.addArguments("--remote-allow-origins=*");
 		WebDriver driver = new ChromeDriver(option);
-		driver.get("https://www.naukri.com/");
+		//driver.get("https://www.naukri.com/");
 
 		/*
 		 * driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
 		 * driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		 */
 		 // driver.get("https://www.nseindia.com/");
+		//driver.get("http://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+		driver.get("https://demo.guru99.com/test/guru99home/");
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		System.out.println(driver.getTitle());
@@ -38,7 +41,7 @@ public class AllAboutWaits {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
 		// 1. Visibility
-		WebElement logInBtn=wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("login_Layer"))));
+		//WebElement logInBtn=wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("login_Layer"))));
 		//logInBtn.click();
 		/*
 		// Try iterating at home
@@ -93,7 +96,7 @@ public class AllAboutWaits {
 				  e.printStackTrace();
 			  }
 		  }
-		   */
+		   
 		//2. Invisibility
 		List<WebElement> lister=driver.findElements(By.xpath("//div[@class='ntc__chips-row']"));
 		JavaScriptExecutorMethods.hideElement(driver,logInBtn);
@@ -105,6 +108,48 @@ public class AllAboutWaits {
 			System.out.println(wait.until(ExpectedConditions.invisibilityOfElementWithText(By.id("login_Layer"),"Login")));
 			
 
+		// 3. Alerts
+		driver.get("https://demo.guru99.com/test/delete_customer.php");
+		driver.findElement(By.xpath("//input[@name='cusid']")).sendKeys("Niks");
+		driver.findElement(By.xpath("//input[@name='submit']")).click();		
+        Alert alert=wait.until(ExpectedConditions.alertIsPresent());
+        Thread.sleep(2000);
+        alert.accept();
+        Thread.sleep(2000);
+        alert.dismiss();
+       
+		
+		// 4. Element selection state
+		
+		System.out.println(wait.until(ExpectedConditions.elementSelectionStateToBe(
+				By.xpath("//input[@value='option-2']"), false)));
+		System.out.println(wait.until(ExpectedConditions.elementSelectionStateToBe(
+				driver.findElement(By.xpath("//input[@value='option-3']")), true)));			
+          
+		// 5. Element Click able state
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='option-2']"))).click();
+		WebElement clickable=wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//input[@value='option-3']"))));
+		clickable.click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='cabbage']"))).click();
+		
+		
+		// 6. Element Selected 
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='option-2']"))).click();
+		System.out.println(wait.until(ExpectedConditions.elementToBeSelected(By.xpath("//input[@value='option-2']"))));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='option-2']"))).click();
+		System.out.println(wait.until(ExpectedConditions.elementToBeSelected(driver.findElement(By.xpath("//input[@value='option-2']")))));
+			*/	
+		// 7. Frame
+		
+		//wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(0));
+		//wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.id("a077aa5e")));		
+		//wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("a077aa5e"));
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(driver.findElement(By.id("a077aa5e"))));		
+		driver.findElement(By.xpath("//a[@href='http://www.guru99.com/live-selenium-project.html']")).click();
+			
+		
 	}
 
 }
